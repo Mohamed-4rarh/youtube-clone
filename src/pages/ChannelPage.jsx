27728590804e7c90867f41cb7b9358c305883952
Mohamed Adham <mainmohedadham@gmail.com>
2023-08-09 02:26:54 +0300

    channel page

--- src/pages/ChannelPage.jsx
+++ src/pages/ChannelPage.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+import {useFetch} from '../hooks/useFetch'
+import { useParams } from "react-router-dom"
 
 const ChannelPage = () => {
+  const [channelData, setChannelData] = useState([])
+  const {channelId} = useParams()
+  useEffect(() => {
+    const params = {
+      part: "snippet,statistics",
+      id: channelId, // channelId
+    };
+    const GetData = async () => {
+      const response = await useFetch(
+        "https://youtube-v31.p.rapidapi.com/channels", params
+      );
+      setChannelData(response.items)
+    }
+    GetData()
+  }, [channelId])
+  console.log(channelData)
   return (
     <div>ChannelPage</div>
   )
